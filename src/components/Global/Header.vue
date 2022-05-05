b73ec6684a7819b22d93926eae07e679260ef20c
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2022-05-05 22:08:58 +0100

    Custom off-canvas menu.

--- src/components/Global/Header.vue
+++ src/components/Global/Header.vue
@@ -1,50 +1,42 @@
 <template>
   <header>
-    <div>
-      Matthew Akino-Wittering
+    <hgroup>
+      <h1>
+        <g-link to="/">
+          Matthew Akino-Wittering
+        </g-link>
+      </h1>
+    </hgroup>
+    <input id="menu-toggle" type="checkbox">
+    <label id="hamburger" for="menu-toggle">
+      <i class="las la-hamburger"></i>
+      <i class="las la-window-close"></i>
+    </label>
+    <section>
+      <label id="dismiss-menu" for="menu-toggle"></label>
       <nav>
-        <div class="grid">
-          <div>
-            <ul class="la-ul">
-              <li>
-                <span class="la-li">
-                  <i class="las la-home"></i>
-                </span>
-                <g-link to="/">Home</g-link>
-              </li>
-              <li>
-                <span class="la-li">
-                  <i class="las la-bullhorn"></i>
-                </span>
-                <g-link to="/blog/">Blog</g-link>
-              </li>
-              <li>
-                <span class="la-li">
-                  <i class="las la-paperclip"></i>
-                </span>
-                <g-link to="/bookmarks/">Bookmarks</g-link>
-              </li>
-              <li>
-                <span class="la-li">
-                  <i class="las la-history"></i>
-                </span>
-                <g-link to="/resume/">Resume</g-link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul class="la-ul">
-              <li class="external-link" v-for="externalLink in $static.allExternalLink.edges" :key="externalLink.node.id">
-                <span class="la-li">
-                  <i class="las la-external-link-square-alt"></i>
-                </span>
-                <a :href="externalLink.node.url" target="_blank">{{ externalLink.node.title }}</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <header>
+          <h2>Menu</h2>
+        </header>
+        <ul>
+          <li>
+            <g-link to="/">Home</g-link>
+          </li>
+          <li>
+            <g-link to="/blog/">Blog</g-link>
+          </li>
+          <li>
+            <g-link to="/bookmarks/">Bookmarks</g-link>
+          </li>
+          <li>
+            <g-link to="/resume/">Resume</g-link>
+          </li>
+          <li class="external-link" v-for="externalLink in $static.allExternalLink.edges" :key="externalLink.node.id">
+            <a :href="externalLink.node.url" target="_blank">{{ externalLink.node.title }}</a>
+          </li>
+        </ul>
       </nav>
-    </div>
+    </section>
   </header>
 </template>
 
