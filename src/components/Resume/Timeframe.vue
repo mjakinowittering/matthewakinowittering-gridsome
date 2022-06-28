287caf2033c01e16c830c628ce59a42ce758c552
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2022-06-28 18:51:38 +0100

    Porting over logic to calculate the length of service.

--- src/components/Resume/Timeframe.vue
+++ src/components/Resume/Timeframe.vue
@@ -1,21 +1,67 @@
 <template>
-  <p class="m-timeframe"><i class="las la-calendar"></i> {{ dateFrom }} - {{ isPresent(dateTo) }} • ELAPSED TIME</p>
+  <p class="m-timeframe"><i class="las la-calendar"></i> {{ dateFrom }} - {{ isPresent(dateTo) }} • {{ calcLengthOfService(dateFrom, dateTo) }}</p>
 </template>
 
 
 <script>
+  import moment from "moment"
+
   export default {
     props: [
       'dateFrom',
       'dateTo'
     ],
     methods: {
+      calcLengthOfService(dateFrom, dateTo) {
+        const lengthOfServiceInMonths = this.calcLengthOfServiceInMonths(
+          moment(dateFrom),
+          dateTo !== null ? moment(dateTo) : moment()
+        )
+
+        if (lengthOfServiceInMonths < 12) {
+          return this. lengthOfServiceLessThanOneYear(lengthOfServiceInMonths)
+        } else if (lengthOfServiceInMonths < 13) {
+          return this. lengthOfServiceOneYear(lengthOfServiceInMonths)
+        } else {
+          return this. lengthOfServiceGreaterThanOneYear(lengthOfServiceInMonths)
+        }
+      },
+      calcLengthOfServiceInMonths(dateFrom, dateTo) {
+        // Calculate the difference in Months as a decimal.
+        // Then truncate to two decimal placed.
+        // Then round up to nearest whole integer.
+        return Math.ceil(dateTo.diff(dateFrom, 'months', true).toFixed(2))
+      },
       isPresent(dateTo) {
         if (dateTo !== null) {
           return dateTo
         } else {
           return 'Present'
         }
+      },
+      lengthOfServiceLessThanOneYear(lengthOfServiceInMonths) {
+        // Calculate difference in Months.
+        const diffInMonths = lengthOfServiceInMonths
+        // Cast suffix for Month unit.
+        const monthSuffix  = (diffInMonths > 1) ? 'mths' : 'mth'
+        // Return length of service.
+        return `${diffInMonths} ${monthSuffix}`
+      },
+      lengthOfServiceOneYear(lengthOfServiceInMonths) {
+        // Return length of service.
+        return `${lengthOfServiceInMonths} yr`
+      },
+      lengthOfServiceGreaterThanOneYear(lengthOfServiceInMonths) {
+        // Calculate difference in Years.
+        const diffInYears  = Math.floor(lengthOfServiceInMonths / 12)
+        // Calculate difference in Months.
+        const diffInMonths = lengthOfServiceInMonths - (diffInYears * 12)
+        // Cast suffix for Month unit.
+        const monthSuffix  = (diffInMonths > 1) ? 'mths' : 'mth'
+        // Cast suffix for Year unit.
+        const yearSuffix   = (diffInYears > 1) ? 'yrs' : 'yr'
+        // Return length of service.
+        return `${diffInYears} ${yearSuffix} ${diffInMonths} ${monthSuffix}`
       }
     }
   }
