9414ff8f6ea909946dce37408ad79b72f3ad5529
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2022-08-16 21:10:35 +0100

    Making the Bookmarks only fill two thirds of the screen.

--- src/components/Bookmarks/Bookmarks.vue
+++ src/components/Bookmarks/Bookmarks.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="m-bookmarks">
     <div class="container">
-      <div v-for="(month, index) in bookmarksByMonth" :key="index">
-        <div class="level">
-          <div class="level-left">
-            <div class="level-item">
-              <h2 class="title is-4 mt-5 mb-2">{{ month.publishedAt }}</h2>
-            </div>
-          </div>
-        </div>
-        <article class="media" v-for="bookmark in month.bookmarks" :key="bookmark.node.id">
-          <figure class="media-left is-hidden-touch">
-            <p class="image is-32x32">
-              <g-image src="~/assets/img/icons/bookmark-video.svg" v-if="bookmark.node.openGraph.ogType == 'video.other'" />
-              <g-image src="~/assets/img/icons/bookmark-article.svg" v-else />
-            </p>
-          </figure>
-          <div class="media-content">
-            <h3 class="title is-4 mb-2">
-              <a :href="bookmark.node.url" target="_blank">{{ bookmark.node.openGraph.ogTitle }}</a>
-            </h3>
-            <div class="content mb-0">
-              <p>{{ bookmark.node.openGraph.ogDescription }}</p>
-            </div>
-            <nav class="level is-mobile">
-              <div class="level-left">
-                <a class="level-item" :href="bookmark.node.url" target="_blank">{{ bookmark.node.domain }}</a>
+      <div class="columns is-multiline">
+        <div class="column is-two-thirds-desktop" v-for="(month, index) in bookmarksByMonth" :key="index">
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <h2 class="title is-4 mt-5 mb-2">{{ month.publishedAt }}</h2>
               </div>
-            </nav>
+            </div>
           </div>
-        </article>
+          <article class="media" v-for="bookmark in month.bookmarks" :key="bookmark.node.id">
+            <figure class="media-left is-hidden-touch">
+              <p class="image is-32x32">
+                <g-image src="~/assets/img/icons/bookmark-video.svg" v-if="bookmark.node.openGraph.ogType == 'video.other'" />
+                <g-image src="~/assets/img/icons/bookmark-article.svg" v-else />
+              </p>
+            </figure>
+            <div class="media-content">
+              <h3 class="title is-4 mb-2">
+                <a :href="bookmark.node.url" target="_blank">{{ bookmark.node.openGraph.ogTitle }}</a>
+              </h3>
+              <div class="content mb-0">
+                <p>{{ bookmark.node.openGraph.ogDescription }}</p>
+              </div>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <a class="level-item" :href="bookmark.node.url" target="_blank">{{ bookmark.node.domain }}</a>
+                </div>
+              </nav>
+            </div>
+          </article>
+        </div>
       </div>
       <Pager class="m-pagination" :info="bookmarks.pageInfo"/>
     </div>
