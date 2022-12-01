e5b1dc2397763cf57238f230229fa640ed1f2609
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2022-12-01 14:38:33 +0000

    Squashing bug caused by Object.fromEntries

--- tools.opengraph.js
+++ tools.opengraph.js
@@ -37,7 +37,7 @@ function crawlFileSystem() {
             const { error, result } = data;
 
             if (error === false) {
-              bookmarkObj.openGraph = Object.fromEntries(
+              bookmarkObj.openGraph = Object.entries(
                 Object.entries(result).filter(([key]) => key.startsWith('og'))
               )
 
